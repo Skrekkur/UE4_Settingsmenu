bbfaa4be3556a6295728763b502240a2c8ed24ea
Aevar Kvaran <skrekkur@gmail.com>
2025-05-09 22:07:44 +0000

    Added source files, Renamed To SettingsMenu since Settings refuses to Build

---
+++ Source/SettingsMenu/SettingsFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SettingsFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SETTINGSMENU_API USettingsFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
